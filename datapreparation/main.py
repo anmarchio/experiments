9a0a3c758b6b8f8748226dc0404fb764450fe515
mara <andreas.margraf@igcv.fraunhofer.de>
2022-09-07 18:33:36 +0200

    insert main file

---
+++ datapreparation/main.py
@@ -0,0 +1,16 @@
+import os
+
+source = os.path.join("C:\\", "Temp")
+
+
+def main():
+    print("Converting images to tiles ...")
+
+    """
+    prefilter using image-browser =>
+    (...)
+    get_data.py -> convert.yp -> filter.py -> (plotly)
+    """
+
+
+main()
