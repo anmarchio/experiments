7fb2dd44f7e2c7666654e71cab4488d857a6e78a
mara <andreas.margraf@igcv.fraunhofer.de>
2022-09-09 16:56:11 +0200

    #5 script for crop to tiles added

--- datapreparation/README.md
+++ datapreparation/README.md
@@ -9,10 +9,10 @@ Doing so will make it easier for helpers to annotate single images. Ideally, the
 
 * Divide image into n tiles of size __1024 x 1024__
 * Numbers are running __from top to bottom__
-* Only tiles which contain __only black values__ are stored on drive (for the sake of space saving)
+* Only tiles which contain __color/grayscale values__ are stored on drive (i.e. plain black images are skipped for the sake of space saving)
 * Store tile in subfolder of running image:
   * if the original image path is: `(...)AirCarbon III\20210324_10h00\9.tif`
-  * the tile path would be: `(...)AirCarbon III\20210324_10h00\9.tif\2.jpg`
+  * the tile path would be: `(...)AirCarbon III\20210324_10h00\t_9.tif\2.jpg`
 * Store metainfo in csv file __(see below)__ (e. g. `20210324_10h00.csv`)
 
 ![Tile Conversion Workflow](tile_conversion_workflow.jpg)
@@ -23,8 +23,8 @@ In order to log the relation between a single tile image to the original large i
 Given an original large image containing 7 tows named `(...)AirCarbon III\20210324_10h00\9.tif`, the log file will be structured as follows:
 
 * Original File: `(...)AirCarbon III\20210324_10h00\9.tif` 
-* Tile filename structure: `(...)AirCarbon III\20210324_10h00\9.tif\[tile#].jpg` 
-* Tile #2: ``(...)AirCarbon III\20210324_10h00\9.tif\2.jpg`` 
+* Tile filename structure: `(...)AirCarbon III\20210324_10h00\t_9.tif\[tile#].jpg` 
+* Tile #2: ``(...)AirCarbon III\20210324_10h00\t_9.tif\2.jpg`` 
 
 The `csv` file would be named `20210324_10h00.csv` and contain the following metadata:
 
---
+++ datapreparation/crop_to_tiles.py
@@ -0,0 +1,81 @@
+import os
+import cv2
+import sys
+import numpy as np
+
+# "C:\\dev\\experiments\\test"
+TILE_HEIGHT = 1024
+TILE_WIDTH = 1024
+
+"""
+    prefilter using image-browser =>
+    (...)
+    get_data.py -> convert.yp -> filter.py -> (plotly)
+"""
+
+
+def convert_tiles(source):
+    file_list = os.listdir(source)
+    for file in file_list:
+        image = cv2.imread(os.path.join(source, file))
+        # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        height, width, channels = image.shape
+
+        """
+        CROP IMAGE
+        """
+        number_of_tiles_height = int(height / TILE_HEIGHT)
+        if height % TILE_HEIGHT > 0:
+            number_of_tiles_height = number_of_tiles_height + 1
+
+        number_of_tiles_width = int(width / TILE_WIDTH)
+        if width % TILE_WIDTH > 0:
+            number_of_tiles_width = number_of_tiles_width + 1
+
+        for col in range(number_of_tiles_width):
+
+            for row in range(number_of_tiles_height):
+                y = row * TILE_HEIGHT
+                x = col * TILE_WIDTH
+
+                h = TILE_HEIGHT
+                w = TILE_WIDTH
+
+                if y < height < y + h:
+                    crop_img = np.zeros((h, w, 3), np.uint8)
+                    tile_img = image[y:height, x:x + w]
+                    crop_img[0:tile_img.shape[0], 0:tile_img.shape[1]] = tile_img
+                elif w < width < x + w:
+                    crop_img = np.zeros((h, w, 3), np.uint8)
+                    tile_img = image[y:y + h, x:width]
+                    crop_img[0:tile_img.shape[0], 0:tile_img.shape[1]] = tile_img
+                else:
+                    crop_img = image[y:y + h, x:x + w]
+
+
+                tile_name = str(col * number_of_tiles_height + row) + ".jpg"
+
+                # Show tile if necessary
+                # cv2.imshow("cropped", crop_img)
+                # cv2.waitKey(0)
+
+                # create dir using the original filename
+                dir_name = os.path.join(source, "t_" + file)
+                if not os.path.isdir(dir_name):
+                    os.mkdir(dir_name, mode=0o777)
+
+                if cv2.countNonZero(crop_img) > 0:
+                    # only store image if it is NOT entirely black
+                    cv2.imwrite(os.path.join(dir_name, tile_name), crop_img)
+
+
+if __name__ == "__main__":
+    print("Converting images to tiles ...")
+    path = ""
+    try:
+        path = sys.argv[1]
+    except Exception as e:
+        print("Argument error: ", e.__class__)
+
+    convert_tiles(path)
