7a84ae8be46a0aea88ff4ece91daf2c5ba8299bc
Hengstmengel <jarl.hengstmengel@igcv.fraunhofer.de>
2022-10-10 16:20:24 +0200

    #9 add json to binary conversion

--- datapreparation/python/json_to_binary.py
+++ datapreparation/python/json_to_binary.py
@@ -3,28 +3,31 @@ import cv2
 import sys
 import json
 import numpy as np
-from matplotlib import pyplot as plt
 
-NROFTILES = 1
 
-def convert_json(source):
+def convert_json(source, destination):
     file_list = os.listdir(source)
     for file in file_list:
         with open(os.path.join(source, file), 'r') as json_file:
             anns = json.load(json_file)
-            img_list = anns.items()
-            print(img_list)
-        #for tile_id in range(NROFTILES):
-            #img_width = anns['images']['width']
-            #img_height = anns
-            #an_area = anns
-
-            #bin_img = np.zeros((img_width, img_height))
-            #bin_img = cv2.fillPoly(bin_img, an_area, (255, 255, 255))
-
-            # cv2.imshow("Binary Image", bin_img)
-            # cv2.waitKey(0)
-
+            for img in anns['images']:
+                bin_img = np.zeros((img['width'], img['height']))
+                img_id = img['id']
+                polygons = []
+                for a in anns['annotations']:
+                    if a['image_id'] == img_id:
+                        polygons.append(a['segmentation'])
+                for poly in polygons:
+                    temp_img = np.zeros((img['width'], img['height']))
+                    i = 1
+                    coord = []
+                    while i < len(poly[0]) + 1:
+                        coord.append((poly[0][i - 1], poly[0][i]))
+                        i += 2
+                    temp_img = cv2.fillPoly(img=temp_img, pts=np.array([coord], dtype=np.int32), color=(255, 255, 255))
+                    bin_img = np.maximum(bin_img, temp_img)
+                path = os.path.join(destination, anns['info']['description'] + '_' + img['file_name'] + '_bin.jpg')
+                cv2.imwrite(path, bin_img)
 
 
 if __name__ == "__main__":
@@ -32,7 +35,8 @@ if __name__ == "__main__":
     path = ""
     try:
         path = sys.argv[1]
+        destination = sys.argv[2]
     except Exception as e:
         print("Argument error: ", e.__class__)
 
-    convert_json(path)
+    convert_json(path, destination)
