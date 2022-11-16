a60ea5937e102a1ba3f62d9b9bac10e4096fa541
samoa <samoa@c-z97-1708.igcv.fraunhofer.de>
2022-11-16 13:15:10 +0100

    #10 add function to convert Pascal VOC xml annotations to binary images

--- datapreparation/python/json_to_binary.py
+++ datapreparation/python/json_to_binary.py
@@ -4,13 +4,17 @@ import sys
 import json
 import string
 import numpy as np
+from imgann import Convertor
 
 
 def convert_json(source, destination):
-    file_list = os.listdir(source)
+    # Iterate through json files and generate the binary images of the annotations of each image
     for file in file_list:
         with open(os.path.join(source, file), 'r') as json_file:
             anns = json.load(json_file)
+            dir_path = os.path.join(destination, anns['info']['description'])
+            if not os.path.exists(dir_path):
+                os.mkdir(dir_path)
             for img in anns['images']:
                 bin_img = np.zeros((img['width'], img['height']))
                 img_id = img['id']
@@ -27,8 +31,36 @@ def convert_json(source, destination):
                         i += 2
                     temp_img = cv2.fillPoly(img=temp_img, pts=np.array([coord], dtype=np.int32), color=(255, 255, 255))
                     bin_img = np.maximum(bin_img, temp_img)
-                path = os.path.join(destination, img['file_name'][:-4] + '.png')
+                path = os.path.join(dir_path, img['file_name'][:-4] + '.png')
                 cv2.imwrite(path, bin_img)
+
+
+def convert_xml(source, destination):
+    # convert xml to json
+    (head, tail) = os.path.split(source)
+    data_dir = os.path.join(head, 'images')
+    json_dir = os.path.join(head, 'ann_json')
+    if not os.path.exists(json_dir):
+        os.mkdir(json_dir)
+    file = os.path.join(json_dir, 'ann_json.json')
+    Convertor.voc2coco(dataset_dir=data_dir, voc_ann_dir=source, save_dir=file)
+
+    # draw binary images of the annotations of each image
+    with open(os.path.join(source, file), 'r') as json_file:
+        anns = json.load(json_file)
+        for img in anns['images']:
+            bin_img = np.zeros((img['width'], img['height']))
+            img_id = img['id']
+            bboxes = []
+            for a in anns['annotations']:
+                if a['image_id'] == img_id:
+                    bboxes.append(a['bbox'])
+            for bbox in bboxes:
+                pt1 = (bbox[0], bbox[1])
+                pt2 = (bbox[2], bbox[3])
+                cv2.rectangle(bin_img, pt1, pt2, (255, 255, 255), -1)
+            path = os.path.join(destination, img['file_name'][:-4] + '.png')
+            cv2.imwrite(path, bin_img)
 
 
 def print_help():
@@ -40,7 +72,7 @@ def print_help():
 
 
 if __name__ == "__main__":
-    print("Converting json to binary image ...")
+    print("Converting to binary image ...")
     path = ""
     destination = ""
     try:
@@ -49,7 +81,14 @@ if __name__ == "__main__":
         else:
             path = sys.argv[1]
             destination = sys.argv[2]
-            convert_json(path, destination)
+            file_list = os.listdir(path)
+
+            if file_list[0].endswith('.xml'):
+                convert_xml(path, destination)
+            else:
+                convert_json(file_list, destination)
+
+
 
     except Exception as e:
         print("Argument error: ", e.__class__)
