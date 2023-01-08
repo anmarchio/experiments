482b839142c07892c4ab56a5393320fd06eed4ec
mara <andreas.margraf@igcv.fraunhofer.de>
2023-01-08 22:28:01 +0100

    Refine and add additional plot styles for evaluation

--- scripts/create_analysis_plots.py
+++ scripts/create_analysis_plots.py
@@ -1,5 +1,8 @@
+import sys
+
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.patches import Polygon
 
 
 def plot_sample():
@@ -26,7 +29,7 @@ def plot_sample():
     plt.show()
 
 
-def plot_fitness_evolution():
+def plot_fitness_evolution(evolutions: list = None):
     fit_max = [0.0, 0.05, 0.21, 0.33, 0.41, 0.55, 0.67, 0.75, 0.75, 0.75, 0.75, 0.75, 0.78, 0.78, 0.82] + 135 * [0.82]
     fit_avg = []
     fit_min = [0.0, 0.01, 0.02, 0.1, 0.15, 0.18] + (len(fit_max) - 6) * [0.18]
@@ -48,5 +51,132 @@ def plot_fitness_evolution():
 
     plt.show()
 
-plot_sample()
-plot_fitness_evolution()
+
+def fancy_mean_plot():
+    Nsteps = 500
+    t = np.arange(Nsteps)
+
+    mu = 0.002
+    sigma = 0.01
+
+    # the steps and position
+    S = mu + sigma * np.random.randn(Nsteps)
+    X = S.cumsum()
+
+    # the 1 sigma upper and lower analytic population bounds
+    lower_bound = mu * t - sigma * np.sqrt(t)
+    upper_bound = mu * t + sigma * np.sqrt(t)
+
+    fig, ax = plt.subplots(1)
+    ax.set_title("Fancy Plot")
+    ax.plot(t, X, lw=2, label='walker position')
+    ax.plot(t, mu * t, lw=1, label='population mean', color='C0', ls='--')
+    ax.fill_between(t, lower_bound, upper_bound, facecolor='C0', alpha=0.4,
+                    label='1 sigma range')
+    ax.legend(loc='upper left')
+
+    # here we use the where argument to only fill the region where the
+    # walker is above the population 1 sigma boundary
+    ax.fill_between(t, upper_bound, X, where=X > upper_bound, fc='red', alpha=0.4)
+    ax.fill_between(t, lower_bound, X, where=X < lower_bound, fc='red', alpha=0.4)
+    ax.set_xlabel('num steps')
+    ax.set_ylabel('position')
+    ax.grid()
+    fig.show()
+
+
+def entropy_fitness_plot():
+    Nsteps = 10
+    t = np.arange(Nsteps)
+
+    # the steps and position
+    S = -1.0 * np.log(0.5 * t) + 2
+    X = np.log(0.5 * t) + 0.5
+
+    fig, ax = plt.subplots(1)
+    ax.set_title("Entropy Fitness Relation")
+
+    ax.plot(t, S, lw=2, label='fitness')
+    ax.plot(t, X, lw=1, label='entropy')
+    ax.fill_between(t, S, X, facecolor='C0', alpha=0.4,
+                    label='overlap')
+    ax.legend(loc='upper left')
+
+    ax.set_xlabel('scenarios')
+    ax.set_ylabel('fitness / entropy')
+    ax.grid()
+    fig.show()
+
+
+def fitness_boxplots():
+    # Random test data
+    np.random.seed(19680801)
+    all_data = [np.random.normal(0, std, size=100) for std in range(1, 4)]
+    labels = ['x1', 'x2', 'x3']
+
+    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(9, 4))
+
+    # rectangular box plot
+    bplot1 = ax1.boxplot(all_data,
+                         vert=True,  # vertical box alignment
+                         patch_artist=True,  # fill with color
+                         labels=labels)  # will be used to label x-ticks
+    ax1.set_title('Rectangular box plot')
+
+    # notch shape box plot
+    bplot2 = ax2.boxplot(all_data,
+                         notch=True,  # notch shape
+                         vert=True,  # vertical box alignment
+                         patch_artist=True,  # fill with color
+                         labels=labels)  # will be used to label x-ticks
+    ax2.set_title('Notched box plot')
+
+    # fill with colors
+    colors = ['pink', 'lightblue', 'lightgreen']
+    for bplot in (bplot1, bplot2):
+        for patch, color in zip(bplot['boxes'], colors):
+            patch.set_facecolor(color)
+
+    # adding horizontal grid lines
+    for ax in [ax1, ax2]:
+        ax.yaxis.grid(True)
+        ax.set_xlabel('Three separate samples')
+        ax.set_ylabel('Observed values')
+
+    plt.show()
+
+
+def computations_per_computing_unit():
+    labels = ['Carbon Fibers', 'Stitched Fibers', 'Pultrusion Resin', 'MVTec Anomaly', 'Steel Welding']
+    fpga_means = [20, 35, 30, 35, 27]
+    cpu_means = [25, 32, 34, 20, 25]
+    fpga_std = [2, 3, 4, 1, 2]
+    cpu_std = [3, 5, 2, 3, 3]
+    width = 0.35  # the width of the bars: can also be len(x) sequence
+
+    fig, ax = plt.subplots()
+
+    ax.bar(labels, fpga_means, width, yerr=fpga_std, label='FPGA')
+    ax.bar(labels, cpu_means, width, yerr=cpu_std, bottom=fpga_means,
+           label='CPU')
+
+    ax.set_ylabel('Percentage')
+    ax.set_title('Percentage of operations by computing unit')
+    ax.legend()
+
+    plt.show()
+
+
+def main() -> int:
+    """Echo the input arguments to standard output"""
+    plot_sample()
+    fancy_mean_plot()
+    plot_fitness_evolution()
+    entropy_fitness_plot()
+    fitness_boxplots()
+    computations_per_computing_unit()
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())  # next section explains the use of sys.exit
