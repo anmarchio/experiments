17270e00068ef40c152901506eb79c3d4d474629
anmarchio <andreas.margraf@gmail.com>
2026-03-22 15:53:26 +0100

    Fix severstal_steel_mean

--- param_tuning/hdev_manual_mean/run_hdev_manual_mean.py
+++ param_tuning/hdev_manual_mean/run_hdev_manual_mean.py
@@ -118,9 +118,9 @@ from param_tuning.hdev_manual_mean.severstal_steel_mean import severstal_steel_m
 MANUAL_HDEV_PIPELINES_MEAN = [
     #"AirCarbon3_80.jpg_bright_mean_pipeline",
     #"AirCarbon3_80.jpg_dark_2_mean_pipeline",
-    "MVTec_AD_Bottle_Broken_Sm_mean_pipeline",
+    #"MVTec_AD_Bottle_Broken_Sm_mean_pipeline",
     #"MVTec_AD_Metal_Nut_mean_pipeline",
-    #"severstal-steel_mean_pipeline",
+    "severstal-steel_mean_pipeline",
 ]
 
 MANUAL_HDEV_PIPELINES_MEAN_TMP = [
--- param_tuning/hdev_manual_mean/severstal_steel_mean.py
+++ param_tuning/hdev_manual_mean/severstal_steel_mean.py
@@ -16,63 +16,47 @@ def get_severstal_steel_mean_pipeline(params, dataset_path=None):
         dataset_path = "/severstal-steel/train_cgp/images"
 
     # Parameters
-    param_lines = "<l>        Filter := '" + str(params[0]) + "'</l>\n" + \
-                  "<l>        Alpha := " + str(params[1]) + "</l>\n" + \
-                  "<l>        Low := " + str(params[2]) + "</l>\n" + \
-                  "<l>        High := " + str(params[3]) + "</l>\n" + \
-                  "<l>        NonMaximumSuppression := '" + str(params[4]) + "'</l>\n" + \
-                  "<l>        Min := " + str(params[5]) + "</l>\n" + \
-                  "<l>        Max := " + str(params[6]) + "</l>\n" + \
-                  "<l>        Iterations := " + str(params[7]) + "</l>\n" + \
-                  "<l>        A := " + str(params[8]) + "</l>\n" + \
-                  "<l>        B := " + str(params[9]) + "</l>\n" + \
+    param_lines = "<l>        MaskHeightBF := " + str(params[0]) + "</l>\n" + \
+                  "<l>        MaskWidthBF := " + str(params[1]) + "</l>\n" + \
+                  "<c>        * reduced values to match MCC = 0.21</c>\n" + \
+                  "<l>        MaskWidth := " + str(params[2]) + "</l>\n" + \
+                  "<l>        MaskHeight := " + str(params[3]) + "</l>\n" + \
+                  "<l>        StdDevScale := " + str(params[4]) + "</l>\n" + \
+                  "<l>        AbsThreshold := " + str(params[5]) + "</l>\n" + \
+                  "<l>        LightDark := '" + params[6] + "'</l>\n" + \
                   "<c></c>\n"
 
     # Core Pipeline Code
-    core_code = "<c>        * EdgesImage</c>\n" \
-                "<l>        edges_image(Image, ImageEdges, ImaDir, Filter, Alpha, NonMaximumSuppression, Low, High)</l>\n" + \
+    core_code = "<c>        * Binomial Filter</c>\n" + \
+                "<l>        binomial_filter(Image, Image, MaskHeightBF, MaskWidthBF)</l>\n" + \
                 "<c></c>\n" + \
-                "<c>        * Threshold</c>\n" + \
-                "<l>        threshold(ImageEdges, RegionThresh, Min, Max)</l>\n" + \
+                "<c>        * VarThreshold</c>\n" + \
+                get_var_threshold_code() + \
                 "<c></c>\n" + \
-                "<c>        * Dilation1 (Ellipse SE)</c>\n" + \
-                "<l>        tuple_max2(A, B, max_rad)</l>\n" + \
-                "<l>        phi := 0.0</l>\n" + \
-                "<l>        longer := A</l>\n" + \
-                "<l>        shorter := B</l>\n" + \
-                "<l>        if (shorter > longer)</l>\n" + \
-                "<l>            tmp := shorter</l>\n" + \
-                "<l>            shorter := longer</l>\n" + \
-                "<l>            longer := tmp</l>\n" + \
-                "<l>        endif</l>\n" + \
-                "<l>        tuple_ceil(max_rad + 1, max_rad_ceil)</l>\n" + \
-                "<l>        gen_ellipse(StructElement, max_rad_ceil, max_rad_ceil, phi, longer, shorter)</l>\n" + \
-                "<l>        dilation1(RegionThresh, StructElement, Region, Iterations)</l>\n" + \
+                "<c>        * Union1</c>\n" + \
+                "<l>        union1(Region, Region)</l>\n" + \
                 "<c></c>\n"
 
     return get_custom_hdev_pipeline_code(pipeline_name, dataset_path, param_lines, core_code)
 
 
 severstal_steel_mean_pipeline_initial_params = [
-    'canny',
-    1.2,
+    17,
     5,
-    20,
-    'nms',
-    65,
-    240,
-    1,
-    18,
-    18
+    29,
+    23,
+    0.2000001,
+    13,
+    "not_equal"
 ]
 
 severstal_steel_bounds = [
     [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37],
     [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37],
-    [v for v in range(3, 30, 2)],
-    [v for v in range(3, 30, 2)],
-    [float(v) / 10.0 for v in range(-10, 10, 1)],
-    [0,128],
+    [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37],
+    [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37],
+    [float(v) * 1.0 for v in range(1, 255, 1)],
+    [float(v) for v in range(1, 255, 1)],
     ['not_equal', 'light', 'equal', 'dark']
 ]
 
